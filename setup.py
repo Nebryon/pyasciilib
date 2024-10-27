2fbd72072091b5145f6ab89b3f94b638eb205444
Alexandre Poggioli <alexandrepoggioli09@gmail.com>
2024-10-27 18:15:50 +0100

    dir added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="pyasciilib",
-    version="1.0.0",
+    version="1.0.1",
     author="Alexandre Poggioli",
     author_email="alexandrepoggioli09@gmail.com",
     description="A library to convert images to ASCII art using different methods",
